1d34e977aeb2dc66eb1fc46639a1bbafbb8eeb69
NadirPozegija <nadirmisimovic@gmail.com>
2026-03-03 20:23:51 -0600

    Initial Commit

---
+++ Aegletes/AegletesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AegletesApp.swift
+//  Aegletes
+//
+//  Created by Nadir Pozegija on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AegletesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
